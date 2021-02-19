e7780f96bffb04241c57f6c016cce88a4a952e1d
Song <quanpengsong@gmail.com>
2021-02-19 10:49:54 +0800

    add resume.sh

---
+++ scripts/resume.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+RESUME_ITER=$1
+
+CUDA_VISIBLE_DEVICES=0 python main.py \
+--mode train --dataset CelebA --image_size 128 --c_dim 100 \
+--cluster_npz_path data/celeba/generated/clusters.npz \
+--dataset_path ../celeba/images \
+--batch_size 32 --selected_labels 4 7 10 11 15 32 \
+--resume_iters $RESUME_ITER
